d70f50adddfdc24efa18f55b7b0b46536356a9c9
yomoda07 <kanji.yy@gmail.com>
2017-11-28 17:47:22 -0500

    Break component into smaller files

---
+++ src/components/Square.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default (props) => (
+  <button className='square' onClick={props.onClick}>
+    {props.value}
+  </button>
+);
--- src/index.js
+++ src/index.js
@@ -1,53 +1,10 @@
 import React from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 import ReactDOM from 'react-dom';
 import './index.css';
-
-function Square(props) {
-  return (
-    <button className='square' onClick={props.onClick}>
-      {props.value}
-    </button>
-  );
-}
-
-class Board extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    };
-  }
-  renderSquare(i) {
-    return (
-      <Square value={this.props.squares[i]}
-              onClick={() => this.props.onClick(i)}
-      />
-    );
-  }
-
-  render() {
-        return (
-      <div>
-        <div className="board-row">
-          {this.renderSquare(0)}
-          {this.renderSquare(1)}
-          {this.renderSquare(2)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(3)}
-          {this.renderSquare(4)}
-          {this.renderSquare(5)}
-        </div>
-        <div className="board-row">
-          {this.renderSquare(6)}
-          {this.renderSquare(7)}
-          {this.renderSquare(8)}
-        </div>
-      </div>
-    );
-  }
-}
+import Board from './components/Board';
+import ticTacToeReducer from './reducers/ticTacToe';
 
 class Game extends React.Component {
   constructor() {
@@ -145,7 +102,13 @@ function calculateWinner(squares) {
 
 // ========================================
 
+const store = createStore(ticTacToeReducer);
+const jsx = (
+  <Provider>
+    <Game />
+  </Provider>
+)
 ReactDOM.render(
-  <Game />,
+  jsx,
   document.getElementById('root')
 );
